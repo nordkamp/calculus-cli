736a60ab8c31454d4d52aa956a81e973d370f94e
Matthew Hoffman <57023230+nordkamp@users.noreply.github.com>
2019-12-03 18:12:54 +1000

    Update MonteCarlov2.py

--- Monte Carlo Approximation/MonteCarlov2.py
+++ Monte Carlo Approximation/MonteCarlov2.py
@@ -1,4 +1,4 @@
-""" Monte-Carlo Integration Approximation Python version 1
+""" Monte-Carlo Integration Approximation Python version 2
     Copyright (C) 2019 Matthew Hoffman (https://github.com/nordkamp/)
 
     This program is free software: you can redistribute it and/or modify
